2d14138f03267f1f27d5f5c9634e44a778e60e28
zephyr <229382777@qq.com>
2018-10-16 18:26:39 +0800

    v2, has jni and hal

--- com_android_server_LedService.cpp
+++ com_android_server_LedService.cpp
@@ -6,55 +6,72 @@
 
 #include <utils/misc.h>
 #include <utils/Log.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <hardware/led_hal.h>
 
 
 namespace android
 {
-    static jint fd;
 
-    jint ledOpen(JNIEnv *env, jobject cls)
-    {
-        fd = open("/dev/zf_led", O_RDWR);//打开设备LED节点
-        ALOGI("native ledOpen : %d", fd);
-        if (fd >= 0)
-            return 0;
-        else
-            return -1;
+static led_device_t* led_device;
+
+jint ledOpen(JNIEnv *env, jobject cls)
+{
+	jint err;
+    hw_module_t* module;
+	hw_device_t* device;
+
+	ALOGI("native ledOpen ...");
+
+	/* 1. hw_get_module */
+    err = hw_get_module("led", (hw_module_t const**)&module);
+    if (err == 0) {
+		/* 2. get device : module->methods->open */
+	    err = module->methods->open(module, NULL, &device);
+	    if (err == 0) {
+			/* 3. call led_open */
+	        led_device = (led_device_t *)device;
+			return led_device->led_open(led_device);
+	    } else {
+	        return -1;
+    	}
     }
+	
+    return -1;	
+}
 
-
-    void ledClose(JNIEnv *env, jobject cls)
-    {
-      ALOGI("native ledClose ...");
-      close(fd);
-    }
-
-    jint ledCtrl(JNIEnv *env, jobject cls, jint which, jint status)
-    {
-        int ret = ioctl(fd, status, which);//给设备节点传输控制信息
-        ALOGI("native ledCtrl : %d, %d, %d", which, status, ret);
-        return ret;
-    }
-
-    /* 定义JNI字段描述符 */
-    static const JNINativeMethod methods[] = {//定义一个映射数组
-        {"native_ledOpen", "()I", (void *)ledOpen},//对应c语言的ledOpen,这个函数没有参数,返回值是int类型
-        {"native_ledClose", "()V", (void *)ledClose},//对应c语言的ledClose,这个函数没有参数,返回值是void类型
-        {"native_ledCtrl", "(II)I", (void *)ledCtrl},//对应c语言的ledCtrl,这个函数有两个int参数,返回值是int类型
-    };
-
-    int register_android_server_LedService(JNIEnv *env)
-    {
-        //注册方法
-        return jniRegisterNativeMethods(env, "com/android/server/LedService",
-                methods, NELEM(methods));
-    }
+void ledClose(JNIEnv *env, jobject cls)
+{
+	//ALOGI("native ledClose ...");
+	//close(fd);
 }
 
 
+jint ledCtrl(JNIEnv *env, jobject cls, jint which, jint status)
+{
+	ALOGI("native ledCtrl %d, %d", which, status);
+	return led_device->led_ctrl(led_device, which, status);
+}
+
+
+static const JNINativeMethod methods[] = {
+	{"native_ledOpen", "()I", (void *)ledOpen},
+	{"native_ledClose", "()V", (void *)ledClose},
+	{"native_ledCtrl", "(II)I", (void *)ledCtrl},
+};
+	
+
+int register_android_server_LedService(JNIEnv *env)
+{
+    return jniRegisterNativeMethods(env, "com/android/server/LedService",
+            methods, NELEM(methods));
+}
+
+}
